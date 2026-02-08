feb04e7fc51f203e87963f370b4774e220bde258
Sonu Rana <sonurana.cse24@sbjit.edu.in>
2026-02-08 10:34:57 +0530

    Implement largest number finder in sonu4.py
    Add a program to find the largest of three numbers based on user input.

---
+++ sonu4.py
@@ -0,0 +1,9 @@
+a=int(input("Enter 1st No: "))
+b=int(input("Enter 2nd No: "))
+c=int(input("Enter 3rd No: "))
+if a>b:
+    print("Largest No:",a)
+elif a<b:
+    print("Largest No:",b)
+else:
+    print("Largest No:",c)
